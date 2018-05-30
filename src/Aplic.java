bcca266b7ad5eb5cae7eb43a0bf89314a7c34536
maran <maran@DESKTOP-B35QRK1>
2018-05-30 17:13:01 -0300

    incluido classe helper

--- src/Aplic.java
+++ src/Aplic.java
@@ -1,3 +1,4 @@
+import fatc.poo.model.GameHelper;
 import fatc.poo.model.SimpleDotCom;
 import java.util.Scanner;
 
@@ -8,24 +9,32 @@ import java.util.Scanner;
 public class Aplic {
 
     public static void main(String[] args) {
-        int randomNum;
-        String userGuess;
+        int numOfGuesses = 0;
         
-        Scanner in = new Scanner(System.in);
+        //criando o objeto da classe auxiliar
+        GameHelper helper = new GameHelper();    
         
-        SimpleDotCom dot = new SimpleDotCom();
+        SimpleDotCom theDotCom = new SimpleDotCom();
         //gera um numero aleatório entre 0 e 5 e armazena na variavel random
-        randomNum = (int) (Math.random()* 5);
+        int randomNum = (int) (Math.random()* 5);
         System.out.println("numero gerado: "+randomNum);
         //cria uma matriz de 1inteiro de tres posições e armazena numeros sequenciais a partir do aleatório
         int[] locations = {randomNum, randomNum+1, randomNum+2};
         //envia a matriz de inteiro para o metodo abaixo
-        dot.setLocationCells(locations);
+        theDotCom.setLocationCells(locations);
+        //criando uma variavel para saber se ta vivo
+        boolean isAlive = true;
         
-        while(dot.getNumOfHits()<3){
-            System.out.println("Digite o Palpite:");
-            userGuess = in.next();        
-            String result = dot.CheckYouself(userGuess);
+        while(isAlive == true){
+           String userGuess = helper.getUserInput("Insira um numero");
+           
+           String result = theDotCom.CheckYouself(userGuess);
+           
+           numOfGuesses++;
+           if (result.equals("kill")){
+               isAlive = false;
+               System.out.println("Voce usou: "+numOfGuesses + " palpites");
+           }
         }
     }
     
